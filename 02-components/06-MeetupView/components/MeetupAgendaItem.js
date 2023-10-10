50d2b49cff5add922087ba68f5757848ac020c8e
Vladimir Merkulov <merkulov.vladimir.67@mail.ru>
2023-10-10 22:18:44 +0300

    02-components_edit

--- 02-components/06-MeetupView/components/MeetupAgendaItem.js
+++ 02-components/06-MeetupView/components/MeetupAgendaItem.js
@@ -7,7 +7,6 @@ export default defineComponent({
 
   props: { 
     agendaItem:{
-     // type: Array
     },  
     },
     methods: {
@@ -15,7 +14,6 @@ export default defineComponent({
         return '/assets/icons/icon-'.concat(agendaItemIcons[`${t}`], '.svg');
         },  
         getTitle(t) { 
-      //    console.log('ciytr', agendaItemDefaultTitles[`${t}`]);
           return agendaItemDefaultTitles[`${t}`];
         },  
 
@@ -26,15 +24,13 @@ export default defineComponent({
     <div class="agenda-item">
       <div class="agenda-item__col">
         <img v-bind:src="getImageUrl(\`\${agendaItem.type}\`)"   class="icon"  /> 
-     <!--   <img src="/assets/icons/icon-key.svg" class="icon" alt="key" /> -->
-      </div>
+       </div>
       <div class="agenda-item__col">{{ agendaItem.startsAt }} - {{ agendaItem.endsAt }}</div>
       <div class="agenda-item__col"> 
         <h3 class="agenda-item__title" v-if="agendaItem.title != null">{{ agendaItem.title }}</h3>
         <h3 class="agenda-item__title" v-else > {{ getTitle(agendaItem.type) }}</h3>
         <p class="agenda-item__talk" v-if="agendaItem.type === 'talk'">
-          <!--  <span>{{ agendaItem.speaker }}</span> -->
-           <span class="agenda-item__dot"> {{ agendaItem.speaker }} </span>  
+          <span class="agenda-item__dot"> {{ agendaItem.speaker }} </span>  
           <span class="agenda-item__lang"> {{ agendaItem.language }} </span>
         </p>
         <p v-if="agendaItem.description !=null">{{ agendaItem.description }}</p>
