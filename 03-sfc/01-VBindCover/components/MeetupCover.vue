9857ca19c796ed6ad5b990790f3059d0c017bdd1
Vladimir Merkulov <merkulov.vladimir.67@mail.ru>
2023-10-25 09:23:19 +0300

    SFC

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+  <div class="meetup-cover" :style="meetup-cover">
+    <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+
+  props: {
+    title: {
+      type: String,
+      required: false,      
+    },
+    image: {
+      type: String,
+      required: false,
+    }
+  },
+  computed: {
+    getImageUrl: function() { 
+        return ' url(' + this.image + ')';
+    }, 
+  },
 };
 </script>
 
@@ -14,8 +30,9 @@ export default {
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),   
+  v-bind(getImageUrl); 
+   /* url('https://course-vue.javascript.ru/api/images/2');*/
   /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
   display: flex;
   flex-direction: column;
@@ -24,6 +41,7 @@ export default {
   height: 410px;
   max-width: 1216px;
   margin: 0 auto;
+  
 }
 
 .meetup-cover__title {
